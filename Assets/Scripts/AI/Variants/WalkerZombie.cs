3ac9583049ed3138a618627f2cc777b0c9492b92
CocaCopa <ioanpand@outlook.com>
2026-04-29 19:11:17 +0300

    AI: Patrol State
    Implemented patrol state and integrated it to WalkerZombie.
    Added patrol points to the scene for testing

--- Assets/Scripts/AI/Variants/WalkerZombie.cs
+++ Assets/Scripts/AI/Variants/WalkerZombie.cs
@@ -5,6 +5,7 @@ namespace HoldMyBeer.AI {
         [Header("Movement")]
         [SerializeField] [Min(0.01f)] private float pathRefreshInterval;
         [SerializeField] [Min(0.01f)] private float stopDistance;
+        [SerializeField] private Transform[] patrolPoints = new Transform[5];
 
         [Header("Idle -> Chase")]
         [SerializeField] [Min(0f)] private float requiredSightTime;
@@ -13,22 +14,35 @@ namespace HoldMyBeer.AI {
         [SerializeField] private float attackTriggerRange;
 
         public override AIStateSetup Compose(AIContext context, AIBrain brain) {
+            Vector3[] patrolVectorPoints = TransformsToVectors(patrolPoints);
             var targetCol = context.Target.GetComponent<Collider>();
 
             var idleState = new IdleMovementState(context);
+            var patrolState = new PatrolAreaState(context, patrolVectorPoints, 0f, 3f, stopDistance);
             var chaseState = new ChaseTargetState(context, pathRefreshInterval, stopDistance);
             var searchState = new SearchTargetState(context, stopDistance);
 
             var idleToChase = new DelayedSightTransition(context.SightStimulus, targetCol, requiredSightTime, chaseState);
             var chaseToSearch = new StateTransition(() => !context.SightStimulus.CanSee(targetCol), searchState);
-            var searchToIdle = new StateTransition(() => searchState.OnLastKnownPos, idleState);
+            var searchToPatrol = new StateTransition(() => searchState.OnLastKnownPos, patrolState);
             var searchToChase = new StateTransition(() => context.SightStimulus.CanSee(targetCol), chaseState);
+            var patrolToChase = new DelayedSightTransition(context.SightStimulus, targetCol, requiredSightTime, chaseState);
 
+            brain.AddMovementTransition(patrolState, patrolToChase);
             brain.AddMovementTransition(idleState, idleToChase);
             brain.AddMovementTransition(chaseState, chaseToSearch);
-            brain.AddMovementTransition(searchState, searchToIdle, searchToChase);
+            brain.AddMovementTransition(searchState, searchToPatrol, searchToChase);
 
-            return new AIStateSetup(idleState, null);
+            return new AIStateSetup(patrolState, null);
+        }
+
+        private static Vector3[] TransformsToVectors(Transform[] transforms) {
+            var vectors = new Vector3[transforms.Length];
+            for (int i = 0; i < vectors.Length; i++) {
+                Vector3 pos = transforms[i].position;
+                vectors[i] = pos;
+            }
+            return vectors;
         }
     }
 }
