f3416808917a920dd74c0ab2ddc53f4580adcce6
TheLoneGameDev <thelonegamedev6@gmail.com>
2026-05-02 23:25:59 +0300

    Buffed Zombie and player
    also added menu when dead

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,27 +1,30 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     [Header("Health Settings")]
     public float maxHealth = 100f;
     public float currentHealth;
-    
+
+    [Header("Death Settings")]
+    public string deathSceneName;
+
     public event Action<float> OnHealthChange;
-    
+
     void Start()
     {
         currentHealth = maxHealth;
         OnHealthChange?.Invoke(currentHealth);
     }
-    
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
         OnHealthChange?.Invoke(currentHealth);
-        
+
         Debug.Log("Player took damage. Health: " + currentHealth);
 
         if (currentHealth <= 0f)
@@ -30,16 +33,23 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    public void Heal(float heal) {
+    public void Heal(float heal)
+    {
         currentHealth += heal;
         currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
         OnHealthChange?.Invoke(currentHealth);
-        
+
         Debug.Log("Player healed. Health: " + currentHealth);
     }
 
     private void Die()
     {
         Debug.Log("Player died!");
+
+        // Unlock and show the mouse
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        SceneManager.LoadScene(deathSceneName);
     }
 }
